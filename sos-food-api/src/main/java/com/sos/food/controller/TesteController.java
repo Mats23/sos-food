4ef37d22383cc0ce281ea0ae1a09f38dcb409763
mats23 <mateus-vinicius19@live.com>
2019-05-29 23:49:31 -0300

    primeiro envio

---
+++ sos-food-api/src/main/java/com/sos/food/controller/TesteController.java
@@ -0,0 +1,15 @@
+package com.sos.food.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteController {
+
+
+    @GetMapping
+    public String teste() {
+        return "Olá mundo";
+    }
+}
